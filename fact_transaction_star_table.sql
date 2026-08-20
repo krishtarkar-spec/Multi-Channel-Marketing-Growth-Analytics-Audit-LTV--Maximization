d620ec1ca3373752f1fafdd7039324c0d7749b36
Krish Tarkar <krishtarkar@gmail.com>
2026-08-20 13:16:05 -0400

    Delete fact_transaction_star_table.sql

--- fact_transaction_star_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE fact_transactions_star AS
-SELECT
-    t.*,
-    u.campaign_id,
-    u.region,
-    u.utm_source AS channel
-FROM fact_transactions t
-JOIN dim_users u ON u.user_id = t.user_id;
